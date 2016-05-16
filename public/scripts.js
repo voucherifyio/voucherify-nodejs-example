ad3236494af4b79f1339b36135bd0c00865ef19f
Jakub Reczko <contact@jakubreczko.com>
2016-05-16 23:45:15 +0200

    Added endpoint which aim to redeem the vouchers

--- public/scripts.js
+++ public/scripts.js
@@ -1,15 +1,43 @@
-(function (window, $, Voucherify) {
+(function (window, $, Voucherify, clientConfig) {
   'use strict'
 
-  const clientConfig = window.clientConfig || {}
-
-  $(function () {
-    Voucherify.initialize(clientConfig.clientApplicationId, clientConfig.clientPublicKey)
-    Voucherify.render('#voucher-checkout', {
-      onValidated: function (response) {
-        const newPrice = Voucherify.utils.calculatePrice(20, response)
-        console.log(newPrice)
-      }
+  const redeemVoucher = function (voucherCode, trackingId) {
+    return $.post('/redeem', {
+      voucher_code: voucherCode,
+      tracking_id: trackingId
     })
+  }
+
+  const getVouchers = function () {
+    return $.get('/vouchers.json')
+  }
+
+  Voucherify.initialize(clientConfig.clientApplicationId, clientConfig.clientPublicKey)
+  Voucherify.setIdentity('gustav@purpleson.com')
+  Voucherify.render('#voucher-checkout', {
+    textPlaceholder: 'e.g. Testing7fjWdr',
+    onValidated: function (res) {
+      const voucherCode = $('#voucher-checkout .voucherifyCode').val()
+      const trackingId = res.tracking_id
+
+      if (res.valid && confirm('Czy chcesz wykorzystać ten Voucher?')) {
+        redeemVoucher(voucherCode, trackingId)
+          .done(function (res) {
+            console.log(res)
+          })
+          .fail(function (err) {
+            console.log(err)
+          })
+      }
+    }
   })
-})(window, window.jQuery, window.Voucherify)
+
+  getVouchers()
+    .done(function (res) {
+      console.log(res)
+    })
+    .fail(function (err) {
+      console.error(err)
+    })
+
+})(window, window.jQuery, window.Voucherify, window.clientConfig)
